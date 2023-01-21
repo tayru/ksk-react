18fe8ba609a1b33f220a9f5e69eb2a1cb2447db3
denis.brudanin@mail.ru <1Fn87jqn>
2023-01-21 10:28:33 +0300

    обновил

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => (
     <div className="container">
         <div className="row">
             <div className="col footer__logo">
-                <img className="footer__logo-img" src="img/a33aeb74-0815-48b0-9c67-abad44ec9999.png" alt=""/>
+                <img className="footer__logo-img" src="/static/img/a33aeb74-0815-48b0-9c67-abad44ec9999.png" alt=""/>
             </div>
             <div className="col footer__text">
                 Ваши заботы – наша работа!
@@ -13,11 +13,11 @@ const Footer = () => (
             </div>
             <div className="col footer__contacts">
                 <div className="footer__phone">
-                    <img className="phone__image" src="img/Call-2.svg" alt=""/> <a href="tel:+79309153808">+7 (930)
+                    <img className="phone__image" src="/staticimg/Call-2.svg" alt=""/> <a href="tel:+79309153808">+7 (930)
                         915-38-08</a>
                 </div>
                 <div className="footer__mail">
-                    <img className="mail__image" src="img/Mail.svg" alt=""/> info@kskpeople.com
+                    <img className="mail__image" src="/staticimg/Mail.svg" alt=""/> info@kskpeople.com
                 </div>
             </div>
         </div>
