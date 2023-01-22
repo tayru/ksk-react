754917b7e9a275911f906f7490ec12103b14db16
tayru <denis.brudanin@mail.ru>
2023-01-22 03:26:24 +0300

    футер

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,18 +6,21 @@ const Footer = () => (
         <div className="row">
             <div className="col footer__logo">
                 <img className="footer__logo-img" src="/static/img/a33aeb74-0815-48b0-9c67-abad44ec9999.png" alt=""/>
+								
             </div>
             <div className="col footer__text">
                 Ваши заботы – наша работа!
-                © 2023 KSK People
+								<div className="col copyright">
+								© 2023 KSK People 
+						</div>
             </div>
+					
             <div className="col footer__contacts">
                 <div className="footer__phone">
-                    <img className="phone__image" src="/staticimg/Call-2.svg" alt=""/> <a href="tel:+79309153808">+7 (930)
-                        915-38-08</a>
+                    <a href="tel:+79309153808">+7 (930) 915-38-08</a>  <img className="phone__image" src="/static/img/Call-2.svg" alt=""/> 
                 </div>
                 <div className="footer__mail">
-                    <img className="mail__image" src="/staticimg/Mail.svg" alt=""/> info@kskpeople.com
+                  info@kskpeople.com  <img className="mail__image" src="/static/img/Mail.svg" alt=""/>
                 </div>
             </div>
         </div>
