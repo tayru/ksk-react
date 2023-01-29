06e0bf1d4c5a47feee965cd71ee5edfa2dade08f
tayru <denis.brudanin@mail.ru>
2023-01-29 20:10:05 +0300

    цвет бургера

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,7 +9,7 @@ const Header = () => (
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                             aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"> </span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto menu">
