6db8a723f9b08218427c65c6f65b6d184243532b
denis.brudanin@mail.ru <1Fn87jqn>
2023-01-29 12:06:02 +0300

    вынес форму в компонент

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Form from "../components/form";
 
 const Main = () => (
     <main className="page1">
@@ -251,37 +252,9 @@ const Main = () => (
                 <img src="/static/img/cover.jpg" alt="cover"/>
             </div>
             <h2 className="feedback-second__title">Нужна помощь?</h2>
-            <div className="feedback-second__block">
-                <h3 className="feedback-second__title2">Свяжитесь с нами, либо оставьте заявку и мы Вам перезвоним</h3>
-                <div className="feedback-second__block-flex">
-                    <div className="feedback-second__block-flex_left">
-                        <input type="text" className="feedback-input" placeholder="Ваше имя"/>
-                        <input type="text" className="feedback-input" placeholder="Телефон"/>
-                        <input type="text" className="feedback-input" placeholder="Элекронная почта"/>
-                        <div className=" feedback-second__button">
-                            <button type="submit" className="btn-default mb-3 button1">Оставить заявку
-                            </button>
-                        </div>
-                        <h2 className="feedback__text">
-                            Нажимая кнопку, вы соглашаетесь на обработку персональных данных
-                        </h2>
-                    </div>
-                    <div className="feedback-second__block-flex_right">
-                        <textarea type="text" className="feedback-input feedback-input__textarea"
-                                  placeholder="Описание запроса"></textarea>
-                        <div className="feedback-second__contacts">
-                            <div className="feedback-second__phone">
-                                <img className="phone__image" src="/static/img/Call-2.svg" alt=""/> <a href="tel:+79309153808">+7
-                                (930) 915-38-08</a>
-                            </div>
-                            <div className="feedback-second__mail">
-                                <img className="mail__image" src="/static/img/Mail.svg" alt=""/> info@kskpeople.com
-                            </div>
 
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Form/>
+
         </div>
 
 
