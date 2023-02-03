c7170fe29b8fc03bd04af60414e3c8bf2777877b
tayru <denis.brudanin@mail.ru>
2023-02-03 19:58:16 +0300

    z

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,6 +6,19 @@ const Header = () => (
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
                     <Link className="navbar-brand" to='/'> <img className="logo-head" src="/static/img/logo-head.png" alt=""/> </Link>
+                    <ul className="navbar-brand-soc-mobile">
+                        <li className="nav-item">
+                            <a className="nav-link nav-link__icon" href="https://t.me/kskpeople" > <img src="/static/img/tg-white.svg" alt="" /></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link nav-link__icon" href="https://wa.me/+79309153808"><img src="/static/img/whatsupp-white.svg" alt="" /></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link nav-link__icon" href="mailto:info@kskpeople.com"><img src="/static/img/mail-white.svg" alt="" /></a>
+                        </li>
+
+                    </ul>
+
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
                             aria-label="Toggle navigation">
@@ -30,17 +43,17 @@ const Header = () => (
                             </li>
                         </ul>
 
-												<ul className="navbar-nav ms-auto menu">
-												<li className="nav-item">
+						<ul className="navbar-nav ms-auto menu">
+							<li className="nav-item">
                                 <a className="nav-link nav-link__contacts" href="tel:+79309153808">+7 (930) 915-38-08</a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item nav-item-soc">
                                 <a className="nav-link nav-link__icon" href="https://t.me/kskpeople" > <img src="/static/img/tg-white.svg" alt="" /></a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item nav-item-soc">
                                 <a className="nav-link nav-link__icon" href="https://wa.me/+79309153808"><img src="/static/img/whatsupp-white.svg" alt="" /></a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item nav-item-soc">
                                 <a className="nav-link nav-link__icon" href="mailto:info@kskpeople.com"><img src="/static/img/mail-white.svg" alt="" /></a>
                             </li>
                             
