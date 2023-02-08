e92f294fe40a861b217057569a5e717d981bdb1d
tayru <denis.brudanin@mail.ru>
2023-02-08 16:13:14 +0300

    слайдер на главный

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -12,29 +12,18 @@ const settings = {
     
 };
  
-const isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
+const getWindowDimensions = function () {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
  
-if (isMobile.any()) {
+if (getWindowDimensions().width <= 767) {
     settings.slidesToShow = 1;
+} else if (getWindowDimensions().width <= 992) {
+    settings.slidesToShow = 2;
 }
  
 const Main = () => (
